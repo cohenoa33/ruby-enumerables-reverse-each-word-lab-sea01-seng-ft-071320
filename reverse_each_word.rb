bc4192843ab4f0afed80dfd38996d3c70e9764cb
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-01 20:24:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word (string)
   array = string.split (" ")
-new_array = []
-array.each {|word| new_array << word.reverse}
-new_array
+  new_array = []
+  array.each do |word| 
+    new_array << word.reverse
+end
+  new_array
 
 end 
