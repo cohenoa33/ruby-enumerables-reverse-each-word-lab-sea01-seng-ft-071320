bb664bd8335ddeea2013d31f61dd4b1ae06bac0f
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-07-01 20:44:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,6 @@ end
   new_array.join (" ")
 end 
 
-def reverse_each_word_collect (string)
-  array = string.split(" ")
- array.collect do |word| 
-    word.reverse
-  end
-  array.join (" ")
+def reverse_each_word (string)
+ string.split(" ").collect { |word| word.reverse}.join(" ")
 end 
